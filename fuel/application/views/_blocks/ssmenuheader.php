d0787515095233b8eeb12971c92ea08f9c849b14
fulgerica2003 <fulgerica2003@gmail.com>
2015-01-15 16:12:42 +0200

    selectie dinamica a limbii
    initial erau trecute in clar; acum se incarca dinamic din array definit
    in my_helper; blocul initial l-am pastrat in _languages.php.old

--- fuel/application/views/_blocks/ssmenuheader.php
+++ fuel/application/views/_blocks/ssmenuheader.php
@@ -4,16 +4,7 @@
 			<a href="<?php echo site_url();?>" id="logo-box"></a>
 			<div id="lang-container">
 				<?php $this->load->view('_blocks/_ssauth')?>
-				<div class="language-box">
-					<ul id="lista-lang">
-						<li><a href="/<?php echo fuel_var('subfolder'); ?>/<?php echo uri_path(); ?>" class="buton-lang<?php echo  $lang == 'ro' ? ' active-lang' : ''; ?>" id="romana">RO</a></li>
-						<li><a href="/<?php echo fuel_var('subfolder'); ?>/it/<?php echo (is_home() ? '' : uri_path()); ?>" class="buton-lang<?php echo  $lang == 'it' ? ' active-lang' : ''; ?>" id="italiana">IT</a></li>
-						<li><a href="/<?php echo fuel_var('subfolder'); ?>/sp/<?php echo (is_home() ? '' : uri_path()); ?>" class="buton-lang<?php echo  $lang == 'sp' ? ' active-lang' : ''; ?>" id="spaniola">SP</a></li>
-						<li><a href="/<?php echo fuel_var('subfolder'); ?>/en/<?php echo (is_home() ? '' : uri_path()); ?>" class="buton-lang<?php echo  $lang == 'en' ? ' active-lang' : ''; ?>" id="engleza">EN</a></li>
-						
-					</ul>
-					<div id="button-lista-lang"></div>
-				</div>
+				<?php $this->load->view('_blocks/_languages')?>
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
 					<span class="sr-only">Toggle navigation</span>
 					<span class="menu-button-red">MENU</span>
