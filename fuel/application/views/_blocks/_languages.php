53f37ef0dea1dbca4a3b932840ec20f58a3ee54c
fulgerica2003 <fulgerica2003@gmail.com>
2015-01-30 16:44:33 +0200

    actualizare meniu limba

--- fuel/application/views/_blocks/_languages.php
+++ fuel/application/views/_blocks/_languages.php
@@ -1,7 +1,7 @@
 <div class="language-box">
 	<ul id="lista-lang">
 		<?php foreach(get_language_options() as $key => $value):?>
-		<li><a href="/<?php echo fuel_var('subfolder'); ?>/<?php echo $key . '/' . (is_home() ? '' : uri_path()); ?>" class="buton-lang<?php echo $lang == $key ? ' active-lang' : ''; ?>" id="<?php echo get_language_name($key)?>"><?php echo $value?></a></li>
+		<li><a href="<?php echo ($key == 'ro' ? '' : $key) . '/' . (is_home() ? '' : uri_path()); ?>" class="buton-lang<?php echo $lang == $key ? ' active-lang' : ''; ?>" id="<?php echo get_language_name($key)?>"><?php echo $value?></a></li>
 		<?php endforeach;?>
 	</ul>
 	<div id="button-lista-lang"></div>
