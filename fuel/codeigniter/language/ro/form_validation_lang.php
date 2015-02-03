c05f05f6efef6644dcb7baa25d30f6148abf3b4e
fulgerica2003 <fulgerica2003@gmail.com>
2015-02-03 18:14:19 +0200

    mesaje validari

--- fuel/codeigniter/language/ro/form_validation_lang.php
+++ fuel/codeigniter/language/ro/form_validation_lang.php
@@ -1,28 +1,28 @@
 <?php
 
 $lang['required']			= "Campul %s este obligatoriu.";
-$lang['isset']				= "The %s field must have a value.";
-$lang['valid_email']		= "The %s field must contain a valid email address.";
-$lang['valid_emails']		= "The %s field must contain all valid email addresses.";
-$lang['valid_url']			= "The %s field must contain a valid URL.";
-$lang['valid_ip']			= "The %s field must contain a valid IP.";
-$lang['min_length']			= "The %s field must be at least %s characters in length.";
-$lang['max_length']			= "The %s field can not exceed %s characters in length.";
-$lang['exact_length']		= "The %s field must be exactly %s characters in length.";
-$lang['alpha']				= "The %s field may only contain alphabetical characters.";
-$lang['alpha_numeric']		= "The %s field may only contain alpha-numeric characters.";
-$lang['alpha_dash']			= "The %s field may only contain alpha-numeric characters, underscores, and dashes.";
-$lang['numeric']			= "The %s field must contain only numbers.";
-$lang['is_numeric']			= "The %s field must contain only numeric characters.";
-$lang['integer']			= "The %s field must contain an integer.";
-$lang['regex_match']		= "The %s field is not in the correct format.";
-$lang['matches']			= "The %s field does not match the %s field.";
-$lang['is_unique'] 			= "The %s field must contain a unique value.";
-$lang['is_natural']			= "The %s field must contain only positive numbers.";
-$lang['is_natural_no_zero']	= "The %s field must contain a number greater than zero.";
-$lang['decimal']			= "The %s field must contain a decimal number.";
-$lang['less_than']			= "The %s field must contain a number less than %s.";
-$lang['greater_than']		= "The %s field must contain a number greater than %s.";
+$lang['isset']				= "Campul %s trebuie sa contina valoare.";
+$lang['valid_email']		= "Campul %s trebuie sa contina adresa valida de email.";
+$lang['valid_emails']		= "Campul %s trebuie sa contina doar adrese valide de email.";
+$lang['valid_url']			= "Campul %s trebuie sa contina un URL valid.";
+$lang['valid_ip']			= "Campul %s trebuie sa contina un IP valid.";
+$lang['min_length']			= "Campul %s trebuie sa contina minim %s caractere.";
+$lang['max_length']			= "Campul %s nu poate depasi %s caractere.";
+$lang['exact_length']		= "Campul %s trebuie sa aiba exact %s caractere.";
+$lang['alpha']				= "Campul %s poate contine doar litere.";
+$lang['alpha_numeric']		= "Campul %s poate contine doar litere si cifre.";
+$lang['alpha_dash']			= "Campul %s poate contine doar litere, cifre, _ si -.";
+$lang['numeric']			= "Campul %s trebuie sa contina doar numere.";
+$lang['is_numeric']			= "Campul %s trebuie sa contina doar caractere numerice.";
+$lang['integer']			= "Campul %s trebuie sa contina un numar intreg.";
+$lang['regex_match']		= "Campul %s nu are formatul corect.";
+$lang['matches']			= "Campul %s nu se potriveste cu campul %s.";
+$lang['is_unique'] 			= "Campul %s trebuie sa contina o valoare unica.";
+$lang['is_natural']			= "Campul %s trebuie sa contina doar numere pozitive.";
+$lang['is_natural_no_zero']	= "Campul %s trebuie sa fie un numar pozitiv.";
+$lang['decimal']			= "Campul %s trebuie sa contina un numar zecimal.";
+$lang['less_than']			= "Campul %s trebuie sa contina un numar < %s.";
+$lang['greater_than']		= "Campul %s trebuie sa contina un numar > %s.";
 
 
 /* End of file form_validation_lang.php */
